e3009b298f4aa18313434e927ff20f9ac0b7a0f3
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-04 11:32:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -204,10 +204,10 @@ if point_on_esr_eo is not None:
         ))
         fig.add_trace(go.Scatter(
             x=[x_orange, x_orange],
-            y=[y_red, 2.60],
+            y=[y_red, 2.65],
             mode='lines',
             line=dict(color='orange', dash='dash'),
-            name='Вертикална линия до y=2.60'
+            name='Вертикална линия до y=2.65'
         ))
 
 # Настройка на графиката
